42b1cf7d029ea6a8d29c72b72d2ea9a0cd244bec
Rémy Garrigue <>
2021-01-04 11:12:13 +0100

    certbot-auto is deprecated, switching to OS packages

--- molecule/default/tests/test_certbot.py
+++ molecule/default/tests/test_certbot.py
@@ -7,7 +7,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("name", ["git", "ssl-cert"])
+@pytest.mark.parametrize(
+    "name", ["python3-certbot", "python3-certbot-dns-route53", "ssl-cert"]
+)
 def test_packages(host, name):
     p = host.package(name)
     assert p.is_installed
@@ -16,16 +18,22 @@ def test_packages(host, name):
 @pytest.mark.parametrize(
     "path",
     [
+        "/etc/letsencrypt/cli.ini",
         "/etc/letsencrypt/hooks/deploy",
         "/etc/letsencrypt/hooks/post",
         "/etc/letsencrypt/hooks/pre",
         "/etc/ssl/certs/ssl-cert-snakeoil.pem",
         "/etc/ssl/private/ssl-cert-snakeoil-combined.pem",
         "/etc/ssl/private/ssl-cert-snakeoil.key",
-        "/opt/certbot/certbot-auto",
-        "/etc/letsencrypt/cli.ini",
+        "/usr/bin/certbot",
     ],
 )
 def test_files(host, path):
     f = host.file(path)
     assert f.exists
+
+
+def test_command(host):
+    with host.sudo():
+        cmd = host.check_output("certbot --version")
+        assert "certbot " in cmd, cmd
